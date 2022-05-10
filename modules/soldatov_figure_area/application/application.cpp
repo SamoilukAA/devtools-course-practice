13fcd276dfb0fb9f4d8e4114177596adf2cf6b18
SamoilukAA <samoyluk.anastasia@gmail.com>
2022-05-10 21:24:59 +0300

    add application

--- modules/soldatov_figure_area/application/application.cpp
+++ modules/soldatov_figure_area/application/application.cpp
@@ -1,3 +1,11 @@
 // Copyright 2022 Samoiluk Anastasiya
 
+#include <iostream>
+#include "include/figure_area_application.h"
 
+int main(int argc, const char** argv) {
+    figureArea_App app;
+    std::string output = app(argc, argv);
+    std::cout << output << std::endl;
+    return 0;
+}
